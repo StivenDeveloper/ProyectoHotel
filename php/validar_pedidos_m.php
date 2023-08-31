5435f680c5df33dc5826194a7602e3a1c455ff29
Weimart24 <weimart24@gmail.com>
2023-08-31 00:17:52 -0500

    se creo otro archivo

---
+++ php/validar_pedidos_m.php
@@ -0,0 +1,41 @@
+<?php   
+    include('conexion_bd.php');
+    $con = conexion();    
+    $cedula= $_POST['cedula'];
+    $nombre= $_POST['nombre'];
+    if(isset($_POST['pedidos'])){
+        $pedidos = $_POST['pedidos'];
+
+        foreach ($pedidos as $productoId => $cantidad) {
+            if($cantidad > 0){
+                $query_consulta = mysqli_query($con,"SELECT precio FROM productos WHERE id_producto = '$productoId'");
+                if($row = $query_consulta->fetch_assoc()){
+                    $total = $row['precio'] * $cantidad;
+                }
+            
+                $query_pedido = mysqli_query($con,"INSERT INTO pedidos (id_producto, cedula_cliente, cantidad, precio_total) VALUES ('$productoId','$cedula','$cantidad','$total')");
+                if(!$query_pedido){
+                      echo "
+                          <script>
+                              alert('***error***.')            
+                              window.location='../menuPrincipal.php';
+                          </script>
+                      ";
+                } 
+            }
+       }
+        echo "
+            <script>
+                alert('Pedido cargado correctamente.')            
+                window.location='../menuPrincipal.php';
+            </script>
+        ";
+        }else{
+            echo"
+                <script>
+                    alert('Seleccione un pedido.')            
+                    window.location='../menuPrincipal.php';
+                </script>
+            ";
+    }
+?>
